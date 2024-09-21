a02ff6e10bc96015b1fbb65ba4e5d1e1736c6738
Milos Milicevic <mmilicev@c1r2p2.42vienna.com>
2024-09-21 12:24:41 +0200

    lstadd_front

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -3,11 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmilicev <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mmilicev <mmilicev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 11:39:49 by mmilicev          #+#    #+#             */
-/*   Updated: 2024/09/21 11:39:52 by mmilicev         ###   ########.fr       */
+/*   Updated: 2024/09/21 12:23:53 by mmilicev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void    ft_lstadd_front(t_list **lst, t_list *new)
+{
+    if (new)
+    {
+        new->next = *lst;
+        lst = &new;
+    }
+}
+#include <stdio.h>
+void print_list(t_list *lst)
+{
+    if (!lst)
+        printf("lst je prazan");
+    while(lst)
+    {
+    printf("%s -> ", (char *)lst->content);
+    lst = lst->next;
+    }
+}
+int main()
+{
+    
+    t_list *first = NULL;
+    t_list *el1 = ft_lstnew("Hello");
+    t_list *el2 = ft_lstnew("World");
+    ft_lstadd_front(&first, el1);
+    ft_lstadd_front(&first, el2);
+    print_list(first);
+}
