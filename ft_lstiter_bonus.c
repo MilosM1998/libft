caffcb284e0038525d98ceb6143ee0af0b078070
Milos Milicevic <mmilicev@c3r8p4.42vienna.com>
2024-09-22 01:13:56 +0200

    added lstiteri

--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mmilicev <mmilicev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 00:30:20 by mmilicev          #+#    #+#             */
-/*   Updated: 2024/09/22 00:48:52 by mmilicev         ###   ########.fr       */
+/*   Updated: 2024/09/22 01:08:35 by mmilicev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,30 +17,21 @@ void	ft_lstiter_bonus(t_list *lst, void (*f)(void *))
 	while (lst)
 	{
 		f(lst->content);
-		lst->next = lst;
+		lst = lst->next;
 	}
 }
-#include <stdio.h>
-int main()
+/* #include <stdio.h>
+void	print_node(void *content)
 {
-    t_list  *test;
-    t_list  *temp;
-    test = NULL;
-    ft_lstadd_front_bonus(&test, ft_lstnew_bonus("hello"));
-    ft_lstadd_back_bonus(&test, ft_lstnew_bonus("world"));
-    temp = test;
-    printf("Before:\n");
-    while(temp)
-    {   
-        printf("%s\n", (char *)temp->content);
-        temp = temp->next;
-    }
-    ft_lstiter_bonus(test, ft_toupper((int)test->content));
-    temp = test;
-    while(temp)
-    {
-        printf("%s\n", (char *)test->content);
-        test = test->next;
-    }
-    
+	printf("%s\n", (char *)content);
 }
+int	main(void)
+{
+	t_list  *test;
+
+	test = NULL;
+	ft_lstadd_front_bonus(&test, ft_lstnew_bonus("hello"));
+	ft_lstadd_back_bonus(&test, ft_lstnew_bonus("world"));
+	ft_lstiter_bonus(test, print_node);
+
+} */
