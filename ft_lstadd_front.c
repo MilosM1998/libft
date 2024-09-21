8c0828fff234c2e329975baa7fe45b51182c9de7
Milos Milicevic <mmilicev@c1r2p2.42vienna.com>
2024-09-21 11:45:29 +0200

    header fix

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -1,35 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmilicev <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/21 11:44:48 by mmilicev          #+#    #+#             */
-/*   Updated: 2024/09/21 11:44:51 by mmilicev         ###   ########.fr       */
+/*   Created: 2024/09/21 11:39:49 by mmilicev          #+#    #+#             */
+/*   Updated: 2024/09/21 11:39:52 by mmilicev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
-{
-	unsigned int	i;
-	char			*str;
-	size_t			s_len;
-
-	i = 0;
-	if (!s)
-		return (NULL);
-	s_len = ft_strlen(s);
-	str = malloc(sizeof(char) * (s_len + 1));
-	if (!str)
-		return (NULL);
-	while (s[i] != '\0')
-	{
-		str[i] = f(i, s[i]);
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
-}
