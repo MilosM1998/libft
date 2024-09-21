2a0e0a43742b228d1b46fcb78ef2c302f0d05236
Milos Milicevic <mmilicev@c1r2p2.42vienna.com>
2024-09-21 11:25:47 +0200

    calloc and makefile fixed

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mmilicev <mmilicev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 20:47:30 by mmilicev          #+#    #+#             */
-/*   Updated: 2024/09/19 16:38:23 by mmilicev         ###   ########.fr       */
+/*   Updated: 2024/09/21 11:22:49 by mmilicev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*res;
-	
+
 	if (nmemb != 0 && size > ((size_t)-1) / nmemb)
 		return (NULL);
 	res = malloc(nmemb * size);
-	if (res == NULL)
+	if (!res)
 		return (NULL);
 	ft_bzero(res, nmemb * size);
 	return (res);
